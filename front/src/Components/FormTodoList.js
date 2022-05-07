71c7ea26988331c962ff7ae56e0542619803b866
Aura Russill <auracris1996@hotmail.com>
2022-05-07 13:16:31 -0500

    DOC - se agrego documentacion en el componente FormTodoList

--- front/src/Components/FormTodoList.js
+++ front/src/Components/FormTodoList.js
@@ -2,28 +2,34 @@ import React, { Fragment, useContext, useRef, useState } from "react";
 import { HOST_API } from "../App";
 import {Store} from './Store'
 
+/**Formulario que genera el grupo de listas de todo */
 export const FormTodoList = () => {
+
   const formRef = useRef(null);
+  // Context
   const {
     dispatch,
     state: { todoList },
   } = useContext(Store);
-  
   const item = todoList.item;
-  const [state, setState] = useState(item);
-  const [habilitado, setHabilitado] = useState(true);
-  const [escribir, setEscribir] = useState(false);
+
+  const [state, setState] = useState(item); //state para un grupo de la lista
+  const [habilitado, setHabilitado] = useState(true);//state para habilitar el botón nueva lista
+  const [escribir, setEscribir] = useState(false);//state que permite escribir en el input
 
   const onAdd = (event) => {
+
     event.preventDefault();
     setHabilitado(true);
     setEscribir(false);
 
+    // request que se enviara al back
     const request = {
       id: null,
       name: state.name,      
     };
 
+    // ruta que conecta con el metodo POST del back
     fetch(HOST_API + "/todolist", {
       method: "POST",
       body: JSON.stringify(request),
@@ -49,8 +55,9 @@ export const FormTodoList = () => {
           placeholder="Nombre de la lista"
           defaultValue={item.name}
           onChange={(event) => {
+            // Validación para que el input no acepte espacios vacíos
             setEscribir(true);
-            setHabilitado(event.target.value.trim().length > 0? false: true);
+            setHabilitado(event.target.value.trim().length > 0? false: true); 
             setState({ ...state, name: event.target.value });
           }}
         ></input>
@@ -62,6 +69,7 @@ export const FormTodoList = () => {
             Nueva Lista
           </button>
         )}
+        {/* Mensaje de validación */}
         {habilitado && escribir && (
           <p>Campo requerido</p>          
         )}
